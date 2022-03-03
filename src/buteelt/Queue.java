ab497a09120844cf0bb2b7de60f6c692425c252d
purev <purev@acer-nitro5>
2022-03-03 16:18:46 +0800

    Lab1-LinkedQueue

---
+++ src/buteelt/Queue.java
@@ -0,0 +1,10 @@
+package buteelt;
+
+public interface Queue
+{
+   public boolean isEmpty();
+   public Object getFrontElement();
+   public Object getRearElement();
+   public void put(Object theObject);
+   public Object remove();
+   }
